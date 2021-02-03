124beb988bc5ad764fb0fb59ac3842f2f6df2dac
abhishek <vishnoi.abhishek@hotmail.com>
2021-02-03 12:06:05 +0530

    first commit

---
+++ src/main/java/com/lizard/edu/camel/eip/multicast/CamelEipMulticastApplication.java
@@ -0,0 +1,13 @@
+package com.lizard.edu.camel.eip.multicast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelEipMulticastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelEipMulticastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lizard/edu/camel/eip/multicast/CamelEipMulticastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizard.edu.camel.eip.multicast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelEipMulticastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
